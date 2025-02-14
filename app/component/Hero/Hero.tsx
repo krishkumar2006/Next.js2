3478b83e04eca766fbbaec9da38159aa5027ebd4
Krish kumar <160249989+krishkumar2006@users.noreply.github.com>
2025-02-14 21:52:46 +0500

    Update Hero.tsx

--- app/component/Hero/Hero.tsx
+++ app/component/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import hero from './hero.module.css'
 const Hero = () => {
   return (
     <main>
-<div className={hero.center}> This is called "HERO SECTION" or main section of th website in which all items appear as per designed website</div>
+<div className={hero.center}> This is called HERO SECTION or main section of th website in which all items appear as per designed website</div>
 
 
     </main>
